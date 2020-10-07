98af4626b9ba6ce9f68fa4a8d1b2cb183e2f9f3a
abhiresearch13 <44253992+abhiresearch13@users.noreply.github.com>
2020-10-07 09:45:16 +0530

    Delete key_tokens.R

--- key_tokens.R
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-ConsumerKey = 'uNwhlfwzcM23fgRAtaoXPvU4d'
-
-ConsumerSecret = 'W8fDQ11WBuVr78Fa9J6mvBLzdJRDSz7Q7Y4YZ4D0OHyjzYsUyq'
-
-OAuthToken = '496808705-hYsursHxOSTbUR1qhYVWwx8LNaLa4X3vB9bsr0TC'
-
-OAuthSecret = 'UNfLDymCr9E3alXyuiPrGmXQQZCV6BOlC8Jw760toNvOZ'
